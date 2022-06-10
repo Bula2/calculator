b5e1ddeca238b24e91d7645c8ae828ee2b5c9ffd
Dmitrii Bulaev <71011407+Bula2@users.noreply.github.com>
2022-06-10 16:44:08 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,16 @@
 const userInput = document.getElementById("userInput");
 let expression = '';
 
+function press_E(num){
+    userInput.value = expression + "E";
+    expression +=num;
+}
+
+function press_Pi(num){
+    userInput.value = expression + "π";
+    expression +=num;
+}
+
 function press(num){
     expression +=num;
     userInput.value = expression;
